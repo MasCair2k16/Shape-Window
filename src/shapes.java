e7ea5368163464194b66a80027a5834b477b3b5a
Peter Tucker <ptucker@whitworth.edu>
2019-01-26 14:17:33 -0800

    Update shapes.java

--- src/shapes.java
+++ src/shapes.java
@@ -1,3 +1,24 @@
+/*
+FEEDBACK
+
+Read from the file correctly: 20/20%
+Implement class hierarchy correctly: 20/20%
+Populate appropriate data structures with shape data: 15/15%
+Show list of shapes correctly: 10/15%
+Show specific shape properties correctly: 15/15%
+Well-structured, well-commented code: 13/15%
+
+
+Comments:
+  interfaceShape doesn't need to implement toString, since that's on Object
+  don't hard-code the image paths
+  Your copy/paste hurt you. Here's an example:
+			for (int i = 0; i < circleList.size(); i++) {
+				if (check == firstW.compareTo(squareList.get(i).toString())) {
+    you are checking circleList.size, so squareList.get(i) is out of range.
+  You don't need MouseMotionListener, since all you implement is mousePressed.
+*/
+
 import java.awt.EventQueue;
 
 import classes.circle;
